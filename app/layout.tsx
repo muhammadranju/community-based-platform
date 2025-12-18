a1740f3510f1b882dbf592b32c88c5bdf16b131d
Muhammad <mdranju23@gmail.com>
2025-12-18 23:49:52 +0600

    feat: Add sonner for toast notifications, integrate it into the root layout, and configure fonts.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "sonner";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
-        <main className="flex-grow">{children}</main>
+        <main className="grow">{children}</main>
+        <Toaster richColors />
       </body>
     </html>
   );
