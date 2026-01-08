4f483d25acc4e0a066151897e8c77c52f9a6a303
Muhammad Ranju <mdranju23@gmail.com>
2026-01-09 01:36:32 +0600

    dest: working deplyment that's why website stop for same hours

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
-        <main className="grow">{children}</main>
+        <main className="grow">
+          {/* {children} */}
+
+          <div className="flex items-center h-screen justify-center  flex-col">
+            <h1 className="text-5xl font-bold">
+              website is under construction
+            </h1>
+            <p className="text-2xl"> Please check back soon </p>
+          </div>
+        </main>
         <ActiveUserTracker />
         <GoogleAnalytics gaId="G-VQ85QD16D X" />
         <Toaster richColors />
